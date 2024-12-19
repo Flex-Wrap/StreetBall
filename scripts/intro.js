3322d3f28612323886eb4f04cc53983a3a928d01
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2024-12-19 07:35:58 +0100

    last try and then I write /StreetBall

--- scripts/intro.js
+++ scripts/intro.js
@@ -2,5 +2,5 @@ import { db } from './firebase.js';
 
 console.log(db);
 setTimeout(() => {
-    window.location.href = "/pages/home.html";  // Redirect to home.html after 2 seconds
+    window.location.href = "./pages/home.html";  // Redirect to home.html after 2 seconds
   }, 2000);
